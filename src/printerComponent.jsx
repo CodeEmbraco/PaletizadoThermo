4dec8e63f827953884bf9cd64c9688afb8d246b0
Sean Axl Garcia Pena <codenidecembraco@gmail.com>
2026-04-06 15:21:18 -0600

    De A a D,
    nada más eso!

--- src/printerComponent.jsx
+++ src/printerComponent.jsx
@@ -288,7 +288,7 @@ export default function PrinterComponent({ barcodeProduct }) {
 
     ^FO300,700^A0R,25,25^FD${part_number}^FS
     ^FO240,700^A0R,25,25^FD${v_serialNo}^FS
-    ^FO180,700^A0R,25,25^FDA^FS
+    ^FO180,700^A0R,25,25^FDD^FS
     ^FO120,700^A0R,25,25^FD${currentDateZPL}^FS
 
     ; Agrega el código QR en la parte derecha
